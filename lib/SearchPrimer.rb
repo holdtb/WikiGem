691033bcd91cbb0ecd6ae72162cffc8ae20709e9
holdtb <Holdt.Bryan@gmail.com>
2015-07-14 17:04:11 -0700

    I caved into ruby idiom peer pressure

--- lib/SearchPrimer.rb
+++ lib/SearchPrimer.rb
@@ -1,7 +1,6 @@
 class SearchPrimer
   def self.prime(search)
     srch = search.join("%20")
-    srch.gsub! /"/, ''
-    return srch
+    srch.gsub! /"/, ""
   end
 end
--- lib/WikiGetter.rb
+++ lib/WikiGetter.rb
@@ -5,7 +5,6 @@ class WikiGetter
   @end_point = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="
 
   def self.get_page_as_json(search)
-    json_result = RestClient.get "#{@end_point}#{search}"
-    json_result
+    RestClient.get "#{@end_point}#{search}"
   end
 end
